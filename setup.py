affab65f222c38ebdd7ad16f3fc21e55acac6353
sergeys78 <info@python.reviews>
2021-06-15 15:00:52 +0300

    Создал setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.read()
+
+setup(
+    name="wsgiRF",
+    version="1.0.0",
+    author="Sergey Sobakin",
+    author_email="info@python.reviews",
+    description="Configurable WSGI REST framework",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pythonapptoall/wsgiRF",
+    packages=['wsgiRF'],
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
