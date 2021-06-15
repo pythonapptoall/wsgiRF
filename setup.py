53ab0c0969038491f42d52577493ffc8171f9958
sergeys78 <info@python.reviews>
2021-06-15 16:06:38 +0300

    Создал setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/pythonapptoall/wsgiRF/",
-    packages=['wsgiRF'],
+    packages=find_packages(),
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.9",
